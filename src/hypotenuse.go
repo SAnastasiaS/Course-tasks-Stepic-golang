84a97304d6de0ba819ec6688d4a5e9bc68f0c914
Samarina Anastasia <a.samarina1997@gmail.com>
2021-08-23 10:47:08 +0300

    add task with finding hypotenuse

---
+++ src/hypotenuse.go
@@ -0,0 +1,25 @@
+//На вход подаются a и b - катеты прямоугольного треугольника. Нужно найти длину гипотенузы
+
+package main
+import (
+    "fmt"
+    "math"
+    "errors"
+)
+
+func hypotenuse (a float64, b float64) float64{
+    if a<=0 || b<=0 {
+        panic(errors.New("Значения катетов не положительные, проверьте входные значения"))
+    }
+    return math.Sqrt(a*a + b*b)
+}
+
+func main(){
+    var a, b float64
+    _, err := fmt.Scan(&a, &b)
+	if err != nil {
+		fmt.Println("Проверьте типы входных параметров")
+	} else {
+		fmt.Println(hypotenuse(a, b))
+	}    
+}
