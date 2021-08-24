79423265d08575dcb20f469d85688a8f4193f63c
Samarina Anastasia <a.samarina1997@gmail.com>
2021-08-24 12:58:12 +0300

    used gofmt

--- src/ShootAndRideBike.go
+++ src/ShootAndRideBike.go
@@ -19,36 +19,38 @@
 Для запуска у себя добавила некоторую самопроверку в main*/
 
 package main
+
 import "fmt"
 
-type GamePerson struct{
-	On bool
+type GamePerson struct {
+	On          bool
 	Ammo, Power int
 }
-func (person *GamePerson) IsAnyAmmo() bool{
-	if person.Ammo>0{
+
+func (person *GamePerson) IsAnyAmmo() bool {
+	if person.Ammo > 0 {
 		return true
 	}
 	return false
 }
 
-func (person *GamePerson) IsAnyPower() bool{
-	if person.Power>0{
+func (person *GamePerson) IsAnyPower() bool {
+	if person.Power > 0 {
 		return true
 	}
 	return false
 }
 
-func (person *GamePerson) Shoot() bool{
-	if person.On && person.IsAnyAmmo(){
+func (person *GamePerson) Shoot() bool {
+	if person.On && person.IsAnyAmmo() {
 		person.Ammo--
 		return true
 	}
 	return false
 }
 
-func (person *GamePerson) RideBike() bool{
-	if person.On && person.IsAnyPower(){
+func (person *GamePerson) RideBike() bool {
+	if person.On && person.IsAnyPower() {
 		person.Power--
 		return true
 	}
@@ -56,24 +58,24 @@ func (person *GamePerson) RideBike() bool{
 }
 
 func main() {
-	testStruct := &GamePerson{true,2,3}
-	if testStruct.On == true{		
-		for testStruct.Ammo!=0 || testStruct.Power!=0{
+	testStruct := &GamePerson{true, 2, 3}
+	if testStruct.On == true {
+		for testStruct.Ammo != 0 || testStruct.Power != 0 {
 			fmt.Println("How many ammo do we have:", testStruct.Ammo, ". How many power do we have:", testStruct.Power)
-			if testStruct.Shoot(){
+			if testStruct.Shoot() {
 				fmt.Println("Shoot")
-			}else{
+			} else {
 				fmt.Println("Ammo is ended")
 			}
-			if testStruct.RideBike(){
+			if testStruct.RideBike() {
 				fmt.Println("Ride a bike")
-			}else{
+			} else {
 				fmt.Println("Power is ended")
 			}
-			continue 
+			continue
 		}
-		fmt.Println("Ammo and power are ended :(")  
-	}else{
-	fmt.Println("We are off :(")
-	}     	
+		fmt.Println("Ammo and power are ended :(")
+	} else {
+		fmt.Println("We are off :(")
+	}
 }
